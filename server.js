f5355f1df1c633e7805a715dd5ebb2e962eb5566
zingxy <1067577595@qq.com>
2023-02-14 00:19:36 +0800

    chore

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ import db from "./app/models/index.js";
 import authRouter from "./app/routes/auth.routes.js";
 import userRouter from "./app/routes/user.routes.js";
 
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-
 /* 连接数据库 */
 db.mongoose.set("strictQuery", "false");
 db.mongoose
@@ -24,21 +21,6 @@ db.mongoose
   });
 
 const app = express();
-
-/* SwaggerApi SwaggerUi */
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Hello World",
-      version: "1.0.0",
-    },
-  },
-  apis: ["./app/routes/*.routes.js"], // files containing annotations as above
-};
-
-const swaggerDocument = swaggerJSDoc(options);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 /* 配置跨源 */
 app.use(
@@ -57,35 +39,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/test", userRouter);
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("Middleware 1 start");
-    next();
-    console.log("Middleware 1 end");
-  },
-  (req, res, next) => {
-    console.log("Middleware 2 start");
-    req.middleware = 2;
-    next();
-    console.log("Middleware 2 end");
-  },
-  (req, res) => {
-    console.log("endpoint start");
-    req.endpoint = 3;
-    res.status(200).send({
-      code: 20000,
-      msg: "sucess",
-    });
-
-    console.log("endpoint end");
-  }
-);
-
-app.use((req, res, next) => {
-  console.log(req.url);
-  next();
-});
 
 app.use((req, res, next) => {
   res.status(404).send({
